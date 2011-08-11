46540c41102ccb5d1aba486a4a1b8bf41c4fee28
wulei <wulei>
2011-08-11 20:59:43 +0800

    spatial ref test scipt

--- spatial_ref_test.c
+++ spatial_ref_test.c
@@ -4,15 +4,21 @@
 int main()
 {
     OGRSpatialReferenceH hSRS = OSRNewSpatialReference(NULL);
-    OSRImportFromEPSG(hSRS, 3857);
+    //OSRImportFromEPSG(hSRS, 3857);
+    OSRSetProjCS(hSRS, "UTM 17(WGS84) in northern hemisphere.");
+    OSRSetWellKnownGeogCS(hSRS, "WGS84");
+    OSRSetUTM(hSRS, 17, TRUE);
 
 
     char *pszWKT = NULL;
 
     OSRExportToWkt(hSRS, &pszWKT);
-
     printf("%s\n", pszWKT);
+    printf("isGeographic: %d\n", OSRIsGeographic(hSRS));
+    printf("isProjected: %d\n", OSRIsProjected(hSRS));
     OGRFree(pszWKT);
+
+    OSRDestroySpatialReference(hSRS);
 
     return 0;
 }
