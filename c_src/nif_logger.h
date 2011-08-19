47f63d208170419a92699fb23a5ac4b1b626172a
mjollnir ray <mjollnir.ray@gmail.com>
2011-08-20 03:18:18 +0800

    auto name log file

--- c_src/nif_logger.h
+++ c_src/nif_logger.h
@@ -1,7 +1,7 @@
 #ifdef DEBUG
     static FILE* logger;
     #define LOG(msg, ...) (fprintf(logger, msg, __VA_ARGS__), fflush(logger))
-    #define OPEN_LOGER() (logger =  fopen("gdal_nifs.log", "a"))
+    #define OPEN_LOGER() (logger =  fopen(__FILE__ ".log", "a"))
     #define CLOSE_LOGER() ( fclose(logger) )
 #else
     #define LOG(msg, ...)
