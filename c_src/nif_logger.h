84cc6ad641f6de1316071ab3fd06b9a6eefa2918
mjollnir ray <mjollnir.ray@gmail.com>
2011-08-20 13:23:17 +0800

    removed log time

--- c_src/nif_logger.h
+++ c_src/nif_logger.h
@@ -1,13 +1,6 @@
 #ifdef DEBUG
-#include <time.h>
-static char* current_time() 
-{
-    time_t now = time(NULL);
-    return ctime(&now);
-}
-
     static FILE* logger;
-    #define LOG(msg, ...) (fprintf(logger, "[%s - Ln:%d] " msg "\n", current_time(), __LINE__, __VA_ARGS__), fflush(logger))
+    #define LOG(msg, ...) (fprintf(logger, "[Ln:%.4d] " msg "\n", __LINE__, __VA_ARGS__), fflush(logger))
     #define OPEN_LOGER() (logger =  fopen(__FILE__ ".log", "a"))
     #define CLOSE_LOGER() ( fclose(logger) )
 #else
