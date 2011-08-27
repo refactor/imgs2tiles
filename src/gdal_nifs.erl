819fda0e441f136007a98209c7448ca21cbec3f8
wulei <wulei>
2011-08-27 21:41:58 +0800

    added rebar.config for rebar support

--- src/gdal_nifs.erl
+++ src/gdal_nifs.erl
@@ -11,7 +11,16 @@
 -on_load(init/0).
 
 init() ->
-    erlang:load_nif("./gdal_nifs", 0).
+%    erlang:load_nif("./gdal_nifs", 0).
+    PrivDir = case code:priv_dir(?MODULE) of
+                {error, bad_name} ->
+                    EbinDir = filename:dirname(code:which(?MODULE)),
+                    AppPath = filename:dirname(EbinDir),
+                    filename:join(AppPath, "priv");
+                Path ->
+                    Path
+            end,
+    erlang:load_nif(filename:join(PrivDir, ?MODULE), 0).
 
 -spec(open(Filename::string()) -> {ok, reference()} | {error, string()}).
 open(Filename) ->
