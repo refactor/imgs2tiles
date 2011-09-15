24b37a57956951ee49a8a7500a9bbe07fabebde7
mjollnir ray <mjollnir.ray@gmail.com>
2011-09-15 22:20:40 +0800

    added README and copyright info

--- src/gdal_nifs.erl
+++ src/gdal_nifs.erl
@@ -1,3 +1,33 @@
+%% -------------------------------------------------------------------
+%% Purpose:  Convert a raster into TMS (Tile Map Service) tiles in a directory or something else.
+%%           - support of global tiles (Spherical Mercator) for compatibility
+%%               with interactive web maps such as Google Maps
+%% 
+%% this is a clone implementent from gdal2tiles.py, but use elang do some parallel work for the speed
+%% gdal2tiles.py is the work of Klokan Petr Pridal, klokan at klokan dot cz
+%%      Web:      http://www.klokan.cz/projects/gdal2tiles/
+%% 
+%% -------------------------------------------------------------------
+%%  Copyright (c) 2011
+%%
+%%   Permission is hereby granted, free of charge, to any person obtaining a
+%%   copy of this software and associated documentation files (the "Software"),
+%%   to deal in the Software without restriction, including without limitation
+%%   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+%%   and/or sell copies of the Software, and to permit persons to whom the
+%%   Software is furnished to do so, subject to the following conditions:
+%%   
+%%   The above copyright notice and this permission notice shall be included
+%%   in all copies or substantial portions of the Software.
+%%   
+%%   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+%%   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+%%   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+%%   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+%%   DEALINGS IN THE SOFTWARE.
+%% -------------------------------------------------------------------
 -module(gdal_nifs).
 
 -export([init/0]).
